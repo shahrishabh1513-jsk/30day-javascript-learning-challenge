0888cda85334d7e344bdc6e7dca5b974aa95183b
shahrishabh1513-jsk <shahrishu1515@gmail.com>
2026-03-19 20:26:19 +0530

    v.1  commit

--- 02. Data_types/string_methods/char_at.js
+++ 02. Data_types/string_methods/char_at.js
@@ -3,4 +3,4 @@ string.charAt(index)
 let string = '30 Days Of JavaScript'
 console.log(string.charAt(0)) // 3
 let lastIndex = string.length - 1
-console.log(string.charAt(lastIndex)) 
+console.log(string.charAt(lastIndex)) // t
